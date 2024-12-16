8e3a2822514b00fc2680402d73c204144ef894d3
akash <akash.prajapati@techqware.com>
2024-12-16 23:20:41 +0530

    CORS header allow for all origin

--- health_care_backend/settings.py
+++ health_care_backend/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
 
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
