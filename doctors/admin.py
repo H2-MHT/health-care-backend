1762036a152e081f6e54acdf874a11114fd0eb57
akash <akash.prajapati@techqware.com>
2025-01-02 18:52:38 +0530

    create review and reply

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Doctor, DoctorNotes
 # Register your models here.
-admin.site.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ['id','user',]
+admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(DoctorNotes)
