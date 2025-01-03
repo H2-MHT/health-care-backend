a027057eb345a4cfdc2346a19fa323c7d74e70d7
akash <akash.prajapati@techqware.com>
2025-01-03 18:05:23 +0530

    reviewer name and profile picture

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -5,11 +5,20 @@ from patients.models import Patient
 from appointments.models import Appointment
 
 class ReviewSerializer(serializers.ModelSerializer):
+    reviewer_name = serializers.SerializerMethodField()
+    reviewer_profile_picture = serializers.SerializerMethodField()
+
     class Meta:
         model = Review
-        fields = ['id', 'doctor', 'rating', 'content', 'recommend', 'created_at']
+        fields = ['id', 'reviewer_name', 'reviewer_profile_picture', 'doctor', 'rating', 'content', 'recommend', 'created_at']
         # read_only_fields = ['reply', 'created_at']
-
+    def get_reviewer_name(self, obj):
+        return obj.patient.user.first_name if obj.patient and obj.patient.user else "Unknown"
+    
+    def get_reviewer_profile_picture(self, obj):
+        if obj.patient and obj.patient.user and hasattr(obj.patient.user, 'profile_picture'):
+            return obj.patient.user.profile_picture.url if obj.patient.user.profile_picture else None
+        return None
     def validate(self, data):
         request = self.context.get('request')
         user = request.user
