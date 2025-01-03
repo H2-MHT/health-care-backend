a0da75f7bc7e9d8c4b50a41e97e4fe06842b7931
akash <akash.prajapati@techqware.com>
2025-01-03 15:23:35 +0530

    reply to review

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,13 +2,13 @@
 from django.urls import path
 from .views import (DoctorReviewsAPIView,
                     ReviewPIView,
-                    # ReplyAPIView,
+                    ReplyAPIView,
 )
 
 urlpatterns = [
     path('doctor/<int:doctor_id>/', DoctorReviewsAPIView.as_view(), name='doctor-reviews'),
     path('create/', ReviewPIView.as_view(), name='review-create'),
     path('view-review/', ReviewPIView.as_view(), name='review-create'),
-    # path('replies/<int:review_id>/', ReplyAPIView.as_view(), name='reply-to-review'),
+    path('replies/<int:review_id>/', ReplyAPIView.as_view(), name='reply-to-review'),
 
 ]
