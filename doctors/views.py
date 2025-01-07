491f532387bd27f62ad14dfe350a00c25134dbfc
akash <akash.prajapati@techqware.com>
2025-01-07 10:43:49 +0530

    get all notes by doctor

--- doctors/views.py
+++ doctors/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from users.models import User
 from .serializers import DoctorNotesSerializer
+from .models import DoctorNotes
 
 class DoctorNotesCreateAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -18,3 +19,13 @@ class DoctorNotesCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, *args, **kwargs):
+        # only doctors can access their notes
+        if request.user.role != "Doctor":
+            return Response({"error": "Only doctors can access their notes."}, status=status.HTTP_403_FORBIDDEN)
+        
+        # Fetch notes created by the logged-in doctor
+        notes = DoctorNotes.objects.filter(doctor=request.user).order_by('-created_at')
+        serializer = DoctorNotesSerializer(notes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
