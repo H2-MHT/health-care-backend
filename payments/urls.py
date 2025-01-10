f5e421f1316b10551b6b436a2d68b449d17a91b3
akash <akash.prajapati@techqware.com>
2025-01-10 15:29:54 +0530

    get all transection detail

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import StripePaymentAPIView
+from .views import (
+    StripePaymentAPIView,
+    TransactionHistoryAPIView,
+)
 
 urlpatterns = [
     path('', StripePaymentAPIView.as_view(), name='stripe-payment'),
+    path("transactions/", TransactionHistoryAPIView.as_view(), name="transactions"),
 ]
