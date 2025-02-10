00b8716dd4e69275fe6fae8a2840d4ff4a3785b7
Akash Kumar Prajapati <akash.prajapati@techqware.com>
2025-02-10 22:42:45 +0530

    clinic api

--- doctors/urls.py
+++ doctors/urls.py
@@ -23,11 +23,13 @@ urlpatterns = [
     path('create-note/', DoctorNotesCreateAPIView.as_view(), name='create_doctor_note'),
     path('doctor-notes/<int:pk>/', DoctorNotesCreateAPIView.as_view(), name='delete_doctor_note'),
     path("get-doctors/", DoctorListAPIView.as_view(), name="doctor-list"),
+    
     # referral and invitation 
     path('referral/generate/', GenerateReferralCodeView.as_view(), name='generate_referral'),
     path('invite/', InviteUserView.as_view(), name='invite_user'),
     path('referral/redeem/<str:invitation_code>/', redeem_invitation, name='redeem_invitation'),
     path('consultation-settings/', ConsultationSettingsAPIView.as_view(), name='create_consultation_settings'),
+    
     # appointment management 
     path('preferences/', AppointmentManagementAPIView.as_view(), name='appointment-preferences'),
     path('user-preferences/', UserPreferenceView.as_view(), name='user-preferences'),
