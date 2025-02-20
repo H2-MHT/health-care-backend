1942f481ec390713f5b71a64f6a4aed657dd0669
Akash Kumar Prajapati <akash.prajapati@techqware.com>
2025-02-20 18:48:51 +0530

    patient-dashboard/get api to fetch all upcoming, competed and canceled appointement

--- patients/urls.py
+++ patients/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import PatientListView
+from .views import(
+    PatientListView,
+    PatientDashboardAPIView,
+)
 
 urlpatterns = [
     path('', PatientListView.as_view()),
+    path('patient-dashboard', PatientDashboardAPIView.as_view()),
 ]
