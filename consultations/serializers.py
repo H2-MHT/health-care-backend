506ef9846c00f82c11da16804f89888f559d6f05
Yashvin Meena <yashvin.meena@techqware.com>
2025-03-11 11:38:27 +0530

    prescription api

---
+++ consultations/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Prescription
+
+class PrescriptionSerializer(serializers.ModelSerializer):
+    notes = serializers.CharField(source='appointment.notes', read_only=False)
+
+    class Meta:
+        model = Prescription
+        fields = ['medicines', 'diagnosis', 'notes', 'additional_instruction']
+
+    def update(self, instance, validated_data):
+        instance.medicines = validated_data.get('medicines', instance.medicines)
+        instance.diagnosis = validated_data.get('diagnosis', instance.diagnosis)
+
+        appointment_data = validated_data.get('appointment', {})
+        if 'notes' in appointment_data:
+            instance.appointment.notes = appointment_data['notes']
+            instance.appointment.save()
+
+        instance.save()
+        return instance
