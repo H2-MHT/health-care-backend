99ae46180a160e9456c49a605b4ea8b2a22bee1a
Yashvin Meena <yashvin.meena@techqware.com>
2025-04-10 11:19:12 +0530

    agora call recording API

--- video_call/urls.py
+++ video_call/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import SendNotificationView, CreateAgoraChatUserAPIView, AgoraUserReceiverIDAPIView
+from .views import SendNotificationView, CreateAgoraChatUserAPIView, AgoraUserReceiverIDAPIView,StartRecordingAPIView, StopRecordingAPIView
 
 urlpatterns = [
     path('send-notification/', SendNotificationView.as_view(), name='send-notification'),
      # agora video call
     path('video-chat/', CreateAgoraChatUserAPIView.as_view(), name='agora-video-chat'),
     # agora receiver user id
-    path('agora-user-receiverid/', AgoraUserReceiverIDAPIView.as_view(), name='agora-receiver-id')
+    path('agora-user-receiverid/', AgoraUserReceiverIDAPIView.as_view(), name='agora-receiver-id'),
+    # start recording
+    path('start-recording/', StartRecordingAPIView.as_view(), name='start-recording'),
+    # stop recording
+    path('stop-recording/', StopRecordingAPIView.as_view(), name='stop-recording'),
+        
 ]
--- video_call/views.py
+++ video_call/views.py
@@ -2,6 +2,8 @@ import os
 import json
 import time
 import random
+import requests
+import base64
 from django.views import View
 from users.models import User
 from django.http import JsonResponse
@@ -14,8 +16,11 @@ import firebase_admin
 from firebase_admin import credentials, messaging
 from agora_token_builder import RtcTokenBuilder
 
+# Agora credentials
 APP_ID = settings.APP_ID
 APP_CERTIFICATE = settings.APP_CERTIFICATE
+AGORA_CUSTOMER_ID = "15a98e4c4e994fbca731b9e0730d9485"
+AGORA_CUSTOMER_SECRET = "2b1d092a24b948219a11feea23ab0fcb"
 
 class FirebaseMessagingService:
     def __init__(self):
@@ -92,6 +97,11 @@ class SendNotificationView(View):
 APP_ID = os.getenv("APP_ID")
 APP_CERTIFICATE = os.getenv("APP_CERTIFICATE")
 EXPIRE_TIME_IN_SECONDS = 3600*10 
+
+AZURE_ACCOUNT_NAME = os.getenv("AZURE_ACCOUNT_NAME")
+AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY")
+AZURE_CONTAINER = os.getenv("AZURE_CONTAINER")
+
 class CreateAgoraChatUserAPIView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
@@ -128,7 +138,15 @@ class CreateAgoraChatUserAPIView(APIView):
 
         expire_timestamp = int(time.time()) + EXPIRE_TIME_IN_SECONDS
 
-        token = RtcTokenBuilder.buildTokenWithUid(
+        senderToken = RtcTokenBuilder.buildTokenWithUid(
+            APP_ID, APP_CERTIFICATE, channel_name, int(senderID), 1, expire_timestamp
+        )
+        
+        receiverToken = RtcTokenBuilder.buildTokenWithUid(
+            APP_ID, APP_CERTIFICATE, channel_name, int(receiverID), 1, expire_timestamp
+        )
+        
+        recorder_token = RtcTokenBuilder.buildTokenWithUid(
             APP_ID, APP_CERTIFICATE, channel_name, 0, 1, expire_timestamp
         )
 
@@ -140,9 +158,9 @@ class CreateAgoraChatUserAPIView(APIView):
         return JsonResponse({
             "app_id": APP_ID,
             "channel": channel_name,
-            "token": token,
-            "currentUser": {"currentUserName": sender.get_full_name(),"uid": sender.id, "firebase_token": currentUserFirebaseToken},
-            "remoteUser": {"remoteUserName": receiver.get_full_name(), "uid": receiver.id,"firebase_token": remoteUserFirebaseToken}
+            "recorder_token": recorder_token,
+            "currentUser": {"currentUserName": sender.get_full_name(),"uid": sender.id,"senderToken": senderToken,"firebase_token": currentUserFirebaseToken},
+            "remoteUser": {"remoteUserName": receiver.get_full_name(), "uid": receiver.id,"receiverToken": receiverToken,"firebase_token": remoteUserFirebaseToken}
         })
 
 class AgoraUserReceiverIDAPIView(APIView):
@@ -180,5 +198,136 @@ class AgoraUserReceiverIDAPIView(APIView):
             return JsonResponse({"agoraReceiverUserUid": user.agoraReceiverUserUid})
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=400)
+  
         
+def get_agora_token(channel_name):
+    url = f"https://api.agora.io/v1/apps/{APP_ID}/cloud_recording/acquire"
+
+    # Encode Customer ID and Secret in Base64
+    credentials = f"{AGORA_CUSTOMER_ID}:{AGORA_CUSTOMER_SECRET}"
+    encoded_credentials = base64.b64encode(credentials.encode()).decode()
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {encoded_credentials}",
+    }
+
+    payload = {
+        "cname": channel_name,
+        "uid": "0",
+        "clientRequest": {},
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()
+
+
+def start_agora_recording(channel_name, resource_id, recorder_token):
+    url = f"https://api.agora.io/v1/apps/{APP_ID}/cloud_recording/resourceid/{resource_id}/mode/mix/start"
+
+    # Encode Customer ID and Secret in Base64
+    credentials = f"{AGORA_CUSTOMER_ID}:{AGORA_CUSTOMER_SECRET}"
+    encoded_credentials = base64.b64encode(credentials.encode()).decode()
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {encoded_credentials}",
+    }
+ 
+    payload = {
+        "cname": channel_name,
+        "uid": "0",
         
+        "clientRequest": {
+            "token": recorder_token,
+            "recordingConfig": {
+                "maxIdleTime": 30,
+                "streamTypes": 2,
+                "channelType": 1,
+                "videoStreamType": 0,
+                "transcodingConfig": {
+                    "width": 1280,
+                    "height": 720,
+                    "fps": 15,
+                    "bitrate": 600,
+                    "maxResolutionUid": "1",
+                    "mixedVideoLayout": 1
+                }
+            },
+            
+            "storageConfig": {
+                "accessKey": AZURE_ACCOUNT_NAME,
+                "region": 0,
+                "bucket": AZURE_CONTAINER,
+                "secretKey": AZURE_ACCOUNT_KEY,
+                "vendor": 4,
+                "fileNamePrefix": ["recordings"]
+            }
+        }
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()
+
+def stop_agora_recording(channel_name, resource_id, sid):
+    url = f"https://api.agora.io/v1/apps/{APP_ID}/cloud_recording/resourceid/{resource_id}/sid/{sid}/mode/mix/stop"
+
+    # Encode Customer ID and Secret in Base64
+    credentials = f"{AGORA_CUSTOMER_ID}:{AGORA_CUSTOMER_SECRET}"
+    encoded_credentials = base64.b64encode(credentials.encode()).decode()
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {encoded_credentials}",
+    }
+
+    payload = {
+        "cname": channel_name,
+        "uid": "0",
+        "clientRequest": {}
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()
+
+class StartRecordingAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        channel_name = request.data.get("channel_name")
+        recorder_token = request.data.get("recorder_token")
+        if not channel_name:
+            return JsonResponse({"error": "Channel name is required"}, status=400)
+
+        try:
+            recording_data = get_agora_token(channel_name)
+            resource_id = recording_data["resourceId"]
+            start_record = start_agora_recording(channel_name, resource_id, recorder_token)
+            sid = start_record["sid"]
+
+            return JsonResponse({
+                "resource_id": resource_id,
+                "sid": sid,
+                "message": "Recording started successfully"
+            })
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+        
+class StopRecordingAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        channel_name = request.data.get("channel_name")
+        resource_id = request.data.get("resource_id")
+        sid = request.data.get("sid")
+
+        if not channel_name or not resource_id or not sid:
+            return JsonResponse({"error": "Missing required fields"}, status=400)
+
+        try:
+            response = stop_agora_recording(channel_name, resource_id, sid)
+            return JsonResponse({"message": "Recording stopped successfully", "data": response})
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
