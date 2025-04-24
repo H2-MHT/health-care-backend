97661ec6c06f1c00924faad2f04993c28eb7144a
Akshat Jain <akshat.jain@techqware.com>
2025-04-24 15:57:13 +0530

    Add media root in profile picture

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from chat.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("chat-room/", ChatRoomAPIView.as_view()),
     path("chat-message/<int:room_id>/", ChatMessageAPIView.as_view()),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
