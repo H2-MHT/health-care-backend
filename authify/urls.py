c2a517e62c41ad8f1ee5dd61ee7c028caee9445d
Akash Kumar Prajapati <akash.prajapati@techqware.com>
2025-05-07 11:59:39 +0530

    fix

--- authify/urls.py
+++ authify/urls.py
@@ -21,10 +21,12 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("signin/", SignInView.as_view(), name="signin"),
     path("logout/", LogoutView.as_view(), name="Logout"),
+    
     # password reset
     path("forget-password/", ForgotPasswordView.as_view(), name="forget-password"),
     path("verify-otp/", OTPVerificationView.as_view(), name="verify-otp"),
     path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
+    
     # update password
     path("change-password/", ChangePasswordView.as_view(), name="change-update-password"),
     path('deactivate-account/', AccountDeactivateDeleteView.as_view(), name='deactivate-account'),
@@ -37,6 +39,6 @@ urlpatterns = [
     
     # firebase device token
     path('firebase-device-token/', UserDeviceTokenAPIView.as_view(), name='firebase-device-token'),
-
     path("view-profile/", GetUserProfileAPIView.as_view(), name="view-profile"),
+    
 ]
