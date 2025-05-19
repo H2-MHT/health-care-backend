4d485da79972b63bfd686493c8f4305f01aeead4
Yashvin Meena <yashvin.meena@techqware.com>
2025-05-19 19:00:00 +0530

    modify working time field

--- clinics/serializers.py
+++ clinics/serializers.py
@@ -5,7 +5,7 @@ import json
 from appointments.serializers import AppointmentSerializer
 
 class ClinicRegisterSerializer(serializers.ModelSerializer):
-    working_time = serializers.CharField(max_length=255)
+    working_time = serializers.CharField(max_length=255,required=False)
     services_provided = serializers.CharField(max_length=255, write_only=True)
     languages = serializers.CharField(max_length=255, write_only=True)
 
